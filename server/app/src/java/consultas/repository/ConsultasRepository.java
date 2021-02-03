3946b1e05108143efddddc486f044a4c7d41d79c
Pedro <pedro.gandra.andrade@gmail.com>
2021-02-03 08:51:03 -0300

    Adicionados todos os servlets de consulta necessários restantes

--- server/app/src/java/consultas/repository/ConsultasRepository.java
+++ server/app/src/java/consultas/repository/ConsultasRepository.java
@@ -51,4 +51,49 @@ public class ConsultasRepository {
 		xml += "</municipio>";
 		return xml;
 	}
+	
+	public String consultarMateria(String id) throws SQLException {
+		String xml = "<materia>";
+		PreparedStatement ps = c.prepareStatement("SELECT * FROM materias WHERE \"id-materia\" = ?");
+		Long idParsed = Long.parseLong(id);
+		ps.setLong(1, idParsed);
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		xml += "<id>" + idParsed + "</id>";
+		xml += "<nome>" + rs.getString("nome") + "</nome>";
+
+		xml += "</materia>";
+		return xml;
+	}
+	
+	public String consultarPreferenciaLocalizacao(String id) throws SQLException {
+		String xml = "<preferencia-localizacao>";
+		PreparedStatement ps = c.prepareStatement("SELECT * FROM preferencialocalizacao WHERE \"id-preferencia-local\" = ?");
+		Long idParsed = Long.parseLong(id);
+		ps.setLong(1, idParsed);
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		xml += "<id>" + idParsed + "</id>";
+		xml += "<descricao>" + rs.getString("descricao") + "</descricao>";
+
+		xml += "</preferencia-localizacao>";
+		return xml;
+	}
+	
+	public String consultarAlunoAvaliaProf(String idAluno, String idProf) throws SQLException {
+		String xml = "<aluno-avalia-prof>";
+		PreparedStatement ps = c.prepareStatement("SELECT * FROM alunoavaliaprof WHERE \"id-aluno\" = ? AND \"id-prof\" = ?");
+		Long idParsedAluno = Long.parseLong(idAluno);
+		Long idParsedProf = Long.parseLong(idProf);
+		ps.setLong(1, idParsedAluno);
+		ps.setLong(2, idParsedProf);
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		xml += "<id-aluno>" + idParsedAluno + "</id-aluno>";
+		xml += "<id-prof>" + idParsedProf + "</id-prof>";
+		xml += "<avaliacao>" + rs.getInt("avaliacao") + "</avaliacao>";
+
+		xml += "</aluno-avalia-prof>";
+		return xml;
+	}
 }
